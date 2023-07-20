774364418a04ded9990ebec8fcc1a6f221b908a0
joselyn127843 <132851581+joselyn127843@users.noreply.github.com>
2023-07-19 23:46:54 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from pymongo import MongoClient
 import pandas as pd
 import datetime
 import sys
+from bson.binary import Binary
+import os
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["Pruebas"]
@@ -17,6 +19,7 @@ collection_factores = db["factores"]
 collection_pagos = db["pagos"]
 collection_fletes = db["fletes"]
 collection_pagos_fletes = db["pagos_fletes"]
+collection_archivos = db["archivos"]
 
 #validaciones
 def validar_fecha(fecha):
@@ -44,11 +47,11 @@ class fletes ():
     def __init__(self,):
         self.pago = self.pagar_fletes()
         self.factura = self.crear_flete()
+        self.archivos = self.archivos_flete()
 
     class crear_flete():
         def __init__(self):
             pass
-
         def crear_flete(self, fecha, serie, folio, proveedor, tipo_cambio, sub_total, iva, retencion_iva, retencion_isr, total, clase, metodo_pago):
             self.fecha = fecha
             self.serie = serie
@@ -169,11 +172,114 @@ class fletes ():
                     "id_flete": id_flete
                 }
                 collection_pagos_fletes.insert_one(pago)
-                print(f"Pago de flete creado para la factura con ID: {id_flete}, serie {self.serie} y folio {self.serie}")
+                print(f"Pago de flete creado para la factura con ID: {id_flete}, serie {self.serie} y folio {self.folio}")
             else:
                 print("No se encontró la factura con el ID especificado")
+
+        def consultar_pagos(self, id_flete):
+            self.id_flete = id_flete
+            self.consultar_flete(id_flete)
+            self.data_pagos = list(collection_pagos_fletes.find({"id_flete": str(id_flete)}))
+            self.suma_pagos = 0
+            self.total = self.data_flete.get("total")
+            
+            if self.data_pagos:
+                for pago in self.data_pagos:
+                    print("-----")
+                    print("\n".join([f"{key}: {value}" for key, value in pago.items()]))
+                    print("-----")
+                    self.suma_pagos += pago.get("monto")
+
+                print(f"Total de pagos: {self.suma_pagos}")
+                print(f"Total de la factura: {self.total}")
+
+                self.saldo = self.total - self.suma_pagos
+                if self.saldo == 0:
+                    print("La factura ya está pagada")
+                elif self.saldo > 0:
+                    print(f"Saldo pendiente: {self.saldo}")
+                elif self.saldo < 0:
+                    print("La factura está sobre pagada")
+                else:
+                    print("Error al calcular el saldo")            
+            else:
+                print("No se encontraron pagos para la factura con el ID especificado")
+
+        def actualizar_pago(self, id_pago, data_new):
+            self.id_pago = id_pago
+            fecha = data_new.get("fecha")
+            banco = data_new.get("banco")
+            monto = data_new.get("monto")
+            refencia = data_new.get("refencia")
+            pedimento = data_new.get("pedimento")
+            tipo_pago = data_new.get("tipo_pago")
+            if validar_fecha(fecha) and validar_numeros([monto]):
+                id_pago = ObjectId(id_pago)
+                data_pago = collection_pagos_fletes.find_one({"_id": id_pago})
+                if data_pago:
+                    data_new = {
+                        "fecha": fecha,
+                        "banco": banco,
+                        "monto": monto,
+                        "refencia": refencia,
+                        "pedimento": pedimento,
+                        "tipo_pago": tipo_pago
+                    }
+                    collection_pagos_fletes.update_one({"_id": id_pago}, {"$set": data_new})
+                    print("\n""Pago actualizado:")
+                    self.consultar_pagos(data_pago.get("id_flete"))
+                else:
+                    print("No se encontró el pago con el ID especificado")
     
-# test facturas
+        def eliminar_pago(self, id_pago):
+            self.id_pago = id_pago
+            id_pago = ObjectId(id_pago)
+            data_pago = collection_pagos_fletes.find_one({"_id": id_pago})
+            if data_pago:
+                collection_pagos_fletes.delete_one({"_id": id_pago})
+                print(f"Pago Elimnado")
+                self.consultar_pagos(data_pago.get("id_flete"))
+            else:
+                print("No se encontró el pago con el ID especificado")
+    
+    class archivos_flete(crear_flete):
+        def __init__(self):
+            super().__init__()
+        def guardar_archivo(self, id_elemento,url):
+            self.id_elemento = id_elemento
+            self.data_archivo = collection_archivos.find_one({"id_elemento": id_elemento})
+
+            if self.data_archivo:
+                self.mensaje = "El archivo ya existe"
+                print(self.mensaje)
+            else:
+                self.consultar_flete(id_elemento)
+                self.nombre_archivo = "archivo-" + id_elemento + ".pdf"
+                with open(url, "rb") as archivo_pdf:
+                    contenido_binario = archivo_pdf.read()
+                documento = {
+                    "id_elemento": id_elemento,
+                    "nombre_archivo": self.nombre_archivo,
+                    "archivo": Binary(contenido_binario)
+                }
+                collection_archivos.insert_one(documento)
+                self.mensaje = (f"archivo del flete con serie {self.serie} y folio {self.folio} guardado")
+                print(self.mensaje)
+
+        def consultar_archivo(self, id_elemento):
+            self.id_elemento = id_elemento
+            self.data_archivo = collection_archivos.find_one({"id_elemento": id_elemento})
+            if self.data_archivo:
+                self.nombre_archivo = self.data_archivo.get("nombre_archivo")
+                self.archivo = self.data_archivo.get("archivo")
+                ruta_descarga = os.path.join("static", "temp", "archivo_descargado.pdf")
+                with open(ruta_descarga, "wb") as archivo_pdf:
+                    archivo_pdf.write(self.archivo)
+                print("archivo descargado")
+            else:
+                print("No se encontró el archivo con el ID especificado")
+    
+# crear pago 
 
 flete = fletes()
 #flete.factura.crear_flete("01-01-2021", "A", "1", "5f9f9b3b9c9d6b3b3c4b4b4b", 20, 100, 16, 0, 0, 116, "flete", "PPD")
@@ -198,5 +304,10 @@ data_new = {
 
 
 # Test de pagos
-#flete.pago.crear_pago_2("64b8766de33fa1c05003d7a8")
-#flete.pago.crear_pago("64b8766de33fa1c05003d7a8", "01-01-2021", "BANAMEX", 100, "VER23-000001", "3002152", "PPD")
+#flete.pago.crear_pago("64b8b33a01bc283c649eb332", "01-01-2021", "BANAMEX", 10, "VER23-000001", "3002152", "PPD")
+#flete.pago.consultar_pagos("64b8b33a01bc283c649eb332")
+#flete.pago.eliminar_pago("64b8b77627091d07946f045e")
+
+# Test de archivos
+#flete.archivos.guardar_archivo("64b8b33a01bc283c649eb332","static/temp/archivo.pdf")
+#flete.archivos.consultar_archivo("64b8b33a01bc283c649eb332")
